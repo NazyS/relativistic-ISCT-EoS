bd05900187407f2dd6361c6c300bd42974fbf4cd
NazyS <nsyakovenko@gmail.com>
2020-12-17 00:59:23 +0200

    improved sp of snd sq with antibaryons incl

--- eos/non_rel_gas.py
+++ eos/non_rel_gas.py
@@ -12,16 +12,39 @@ class Non_Rel_Gas(Eq_of_state):
         self.g = g
         self.m = m
 
-    num_of_eq_in_eos = 1
+        self.num_of_eq_in_eos = 1
+        self.num_of_components = 1
+
+    # def p_term(self, T, mu):
+    #     return self.g * T * (self.m*T/(2*np.pi*hbar**2))**1.5 * np.exp((mu-self.m)/T)
+
+    # def p_total(self, T, mu):
+    #     return self.p_term(T, mu) + self.p_term(T, -mu)
+
+    def p_total(self, T, mu):
+        return self.g * T * (self.m*T/(2*np.pi*hbar**2))**1.5 * np.exp((-self.m)/T) * np.cosh(mu/T)
 
     def EoS(self, p, T, mu):
-        return self.g * T * (self.m*T/(2*np.pi*hbar**2))**1.5 * np.exp(-self.m/T) - p
+        return p - self.p_total(T, mu)
 
-    def Xi_with_log(self, T, mu):
-        return derivative(lambda T: np.log(self.entropy(T, mu)), T, dx=1e-6)
+    def entr_analyt(self, T, mu):
+        p = self.p_eq(T, mu)
+        return p/T*(5./2. - mu/T*np.tanh(mu/T) + self.m/T)
 
-    def Xi(self, T, mu):
-        return derivative(lambda T: self.entropy(T, mu), T, dx=1e-6)/self.entropy(T, mu)
+    def dens_analyt(self, T, mu):
+        p = self.p_eq(T, mu)
+        return p/T*np.tanh(mu/T)
 
-    def speed_of_s_sq_Xi(self, T, mu):
-        return 1/T/self.Xi(T, mu)
+    def energy_analyt(self, T, mu):
+        p = self.p_eq(T, mu)
+        return p*(1.5 + self.m/T)
+
+    # analytical expression for mu = 0 and no antibaryons
+    def Xi_with_log(self, T):
+        return derivative(lambda T: np.log(self.entropy(T, 0)), T, dx=1e-6)
+
+    def Xi(self, T, **kwargs):
+        return derivative(lambda T: self.entropy(T, 0, **kwargs), T, **kwargs)/self.entropy(T, 0, **kwargs)
+
+    def speed_of_s_sq_Xi(self, T, **kwargs):
+        return 1/T/self.Xi(T, **kwargs)
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from scipy.misc import derivative
 from functools import lru_cache
 
 
-def partial_derivative(func, var=0, point=[], dx=1., order=3, **kwargs):
+def partial_derivative(func, var=0, point=[], dx=1e-1, order=3, **kwargs):
     args = point[:]
 
     def wraps(x):
@@ -16,121 +16,129 @@ def partial_derivative(func, var=0, point=[], dx=1., order=3, **kwargs):
     return derivative(wraps, point[var], dx=dx, order=order)
 
 
-hbar = 197.326
+hbar = 197.3269804
 
-
+# thermal density function
 @lru_cache(maxsize=32)
 def phi(T, m):
     def func(p):
         return p**2 * np.exp(-np.sqrt(p**2+m**2)/T)
-
     integration = quad(func, 0, np.inf)[0]
     return (4*np.pi)/(2*np.pi*hbar)**3 * integration
 
 
-# # Main equation of state Class
-
+# Main equation of state Class
 class Eq_of_state:
-
-    # def __init__(self):
-    #     self.num_of_eq_in_eos = num_of_eq_in_eos
 
     @lru_cache(maxsize=512)
     def p_eq(self, T, *mu, init=None, format='p only', **kwargs):
-        init = init if init else [0.]*self.num_of_eq_in_eos
+        init = init if init else np.full(self.num_of_eq_in_eos, 0.)
         root = fsolve(self.EoS, init, args=(T, *mu),
-                      maxfev=10000,
+                      maxfev=10000
                       )
         diff = self.EoS(root, T, *mu)
         if sum(diff[i]**2/root[i]**2 for i in range(len(diff))) > 1e-4:
             print('error for T={}, mu={}'.format(T, mu[0]))
-            return np.nan if format == 'p only' else np.array([np.nan]*len(init))
-        # print(root)
+            return np.nan if format == 'p only' else np.full(self.num_of_eq_in_eos, np.nan)
         return root[0] if format == 'p only' else root
 
-    #     entropy density
+    # entropy density
     def entropy(self, T, *mu, **kwargs):
         return partial_derivative(self.p_eq, 0, [T, *mu], **kwargs)
 
+    # baryon density of a single component
+    def density_baryon_comp(self, comp, T, *mu, **kwargs):
+        return partial_derivative(self.p_eq, 1+comp, [T, *mu], **kwargs)
+
+    def density_baryon(self, T, *mu, **kwargs):
+        return sum(self.density_baryon_comp(i, T, *mu, **kwargs) for i in range(len(mu)))
+
     # particle density of single component
-    @lru_cache(maxsize=512)
-    def density_comp(self, comp_number, T, *mu, **kwargs):
-        return partial_derivative(self.p_eq, 1+comp_number, [T, *mu], **kwargs)
+    # @lru_cache(maxsize=512)
+    # def density_comp(self, comp_number, T, *mu, **kwargs):
+    #     return partial_derivative(self.p_eq, 1+comp_number, [T, *mu], **kwargs)
 
     # baryon minus its imaginary antibaryon density for each component
     # 0.5 factor due to same input from actual antibaryon term with opposite sign in total baryon charge density
-    def baryon_minus_antibaryon_comp_density_halfed(self, comp_number, T, *mu, **kwargs):
-        mu_inverted = tuple([-el for el in mu])
-        return 0.5*(self.density_comp(comp_number, T, *mu, **kwargs) - self.density_comp(comp_number, T, *mu_inverted, **kwargs))
+    # def baryon_minus_antibaryon_comp_density_halfed(self, comp_number, T, *mu, **kwargs):
+    #     mu_inverted = tuple([-el for el in mu])
+    #     return 0.5*(self.density_comp(comp_number, T, *mu, **kwargs) - self.density_comp(comp_number, T, *mu_inverted, **kwargs))
 
     # baryon charge density which is baryon density minus antibaryon charge density
-    def density_baryon(self, T, *mu, **kwargs):
-        return sum(np.sign(mu)[i] * self.baryon_minus_antibaryon_comp_density_halfed(i, T, *mu, **kwargs) for i in range(self.num_of_components))
-        # return self.density_comp(1, T, *mu, **kwargs)
+    # def density_baryon(self, T, *mu, **kwargs):
+    #     return sum(np.sign(mu)[i] * self.baryon_minus_antibaryon_comp_density_halfed(i, T, *mu, **kwargs) for i in range(self.num_of_components))
 
     # total particle density
     # def density(self, T, *mu, **kwargs):
     #     return sum(self.density_comp(i, T, *mu, **kwargs) for i in range(self.num_of_components))
 
+    # def energy(self, T, *mu, **kwargs):
+    #     return T*self.entropy(T, *mu, **kwargs) + sum(mu[i]*self.baryon_minus_antibaryon_comp_density_halfed(i, T, *mu, **kwargs) for i in range(self.num_of_components)) - self.p_eq(T, *mu, **kwargs)
+
     def energy(self, T, *mu, **kwargs):
-        # return T*self.entropy(T, *mu) + sum( mu[i]*self.density_comp(i, T, *mu) for i in range(self.num_of_components) ) - self.p_eq(T,*mu)  # for multi mu_k case
-        return T*self.entropy(T, *mu, **kwargs) + sum(mu[i]*self.baryon_minus_antibaryon_comp_density_halfed(i, T, *mu, **kwargs) for i in range(self.num_of_components)) - self.p_eq(T, *mu, **kwargs)
+        return T*self.entropy(T, *mu, **kwargs) + sum(mu[i]*self.density_baryon_comp(i, T, *mu, **kwargs) for i in range(len(mu))) - self.p_eq(T, *mu, **kwargs)
 
     # entropy by baryon density ratio ( \sigma  = s / n_B )
-    def entr_by_density_baryon(self, T, *mu, **kwargs):
+    def sigma(self, T, *mu, **kwargs):
         return self.entropy(T, *mu, **kwargs)/self.density_baryon(T, *mu, **kwargs)
 
-    def speed_of_s_sq(self, T, *mu, **kwargs):
+    def speed_of_s_sq(self, T, *mu, warning=False, **kwargs):
 
         def d_mu_by_d_T(T, *mu):
-            num = partial_derivative(
-                self.entr_by_density_baryon, 0, [T, *mu], **kwargs)
-            denum = partial_derivative(
-                self.entr_by_density_baryon, 1, [T, *mu], **kwargs)
-            # dealing with infinite sigma if \mu = 0 hence baryonic density = 0
+            num = partial_derivative(self.sigma, 0, [T, *mu], **kwargs)
+            denum = partial_derivative(self.sigma, 1, [T, *mu], **kwargs)
+            res = -num/denum
+
+            # dealing with infinite sigma if \mu = 0 hence baryonic density = 0 and constant
             # might be dangerous
-            if np.isnan(num/denum):
+            if np.isnan(res):
+                if warning:
+                    print('nan value warning for T={}'.format(T))
                 return 0.
             else:
-                return num/denum
+                return res
 
         d_mu_d_T = d_mu_by_d_T(T, *mu)
 
-        num = partial_derivative(self.p_eq, 0, [T, *mu], **kwargs) - partial_derivative(
-            self.p_eq, 1, [T, *mu], **kwargs)*d_mu_d_T
-        denum = partial_derivative(self.energy, 0, [T, *mu], **kwargs) - partial_derivative(self.energy, 1, [
-            T, *mu], **kwargs)*d_mu_d_T
+        num = self.entropy(T, *mu, **kwargs) + self.density_baryon(T, *mu, **kwargs)*d_mu_d_T
+        denum = partial_derivative(self.energy, 0, [T, *mu], **kwargs) + \
+            partial_derivative(self.energy, 1, [T, *mu], **kwargs)*d_mu_d_T
         return num/denum
 
     # speed of sound for multiple chemical potentials
-    def speed_of_s_sq_MULT(self, T, *mu, **kwargs):
+    # NOT TESTED
+    def speed_of_s_sq_MULT(self, T, *mu, warning=False, **kwargs):
 
         def deriv_by_sigma_with_const_T(func, T, *mu):
-            num = sum(partial_derivative(
-                func, comp+1, [T, *mu], **kwargs) for comp in range(self.num_of_components))
-            denum = sum(partial_derivative(self.entr_by_density_baryon, comp+1,
+            num = sum(partial_derivative(func, comp+1, [T, *mu], **kwargs) for comp in range(self.num_of_components))
+            denum = sum(partial_derivative(self.sigma, comp+1,
                                            [T, *mu], **kwargs) for comp in range(self.num_of_components))
-            # dealing with infinite sigma if \mu = 0 hence baryonic density = 0
+            res = num/denum
+
+            # dealing with infinite sigma if \mu = 0 hence baryonic density = 0 and constant
             # might be dangerous
-            if np.isnan(num/denum):
+            if np.isnan(res):
+                if warning:
+                    print('nan value warning for T={}'.format(T))
                 return 0.
             else:
-                return num/denum
+                return res
 
         def sigma_deriv_by_T(T, *mu):
-            val = partial_derivative(
-                self.entr_by_density_baryon, 0, [T, *mu], **kwargs)
-            # dealing with infinite sigma if \mu = 0 hence baryonic density = 0
+            val = partial_derivative(self.sigma, 0, [T, *mu], **kwargs)
+            # dealing with infinite sigma if \mu = 0 hence baryonic density = 0 and constant
             # might be dangerous
             if np.isnan(val):
+                if warning:
+                    print('nan value warning for T={}'.format(T))
                 return 0.
             else:
                 return val
 
         sigma_deriv = sigma_deriv_by_T(T, *mu)
 
-        num = partial_derivative(self.p_eq, 0, [
-                                 T, *mu], **kwargs) - deriv_by_sigma_with_const_T(self.p_eq, T, *mu)*sigma_deriv
-        denum = partial_derivative(self.energy, 0, [
-                                   T, *mu], **kwargs) - deriv_by_sigma_with_const_T(self.energy, T, *mu)*sigma_deriv
+        num = partial_derivative(self.p_eq, 0, [T, *mu], **kwargs) - \
+            deriv_by_sigma_with_const_T(self.p_eq, T, *mu)*sigma_deriv
+        denum = partial_derivative(self.energy, 0, [T, *mu], **kwargs) - \
+            deriv_by_sigma_with_const_T(self.energy, T, *mu)*sigma_deriv
         return num/denum
