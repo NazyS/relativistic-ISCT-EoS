9e27f9ab3f57abadf6a527eea7b104ebab906a25
NazyS <nsyakovenko@gmail.com>
2020-12-17 02:33:47 +0200

    rel ISCT with antibar incl and multicomp support

--- eos/relativistic_ISCT.py
+++ eos/relativistic_ISCT.py
@@ -8,9 +8,9 @@ from main import Eq_of_state, hbar
 class Relativistic_ISCT(Eq_of_state):
     def __init__(self, alpha=1., beta=1., m=940., R=0.5, g=3., alpha_p=1., beta_p=1., a=0.775, b=1., eos=None, components=1):
         self.num_of_components = components
-        self.m = m
-        self.R = R
-        self.g = g
+        self.m = self.make_parameter_array(m)
+        self.R = self.make_parameter_array(R)
+        self.g = self.make_parameter_array(g)
         self.b = b
 
         if eos == 'vdw':
@@ -47,65 +47,90 @@ class Relativistic_ISCT(Eq_of_state):
 
         self.num_of_eq_in_eos = 3
 
-    def delta_c1(self, theta):
+    # function to make parameter array of expected size
+    def make_parameter_array(self, in_values):
+        input = np.array([in_values]).flatten()
+        if len(input) == self.num_of_components:
+            return input
+        else:
+            if len(input) == 1:
+                return np.repeat(input, self.num_of_components)
+            else:
+                raise Exception('Wrong parameters format')
+
+    @staticmethod
+    def delta_c1(theta):
         return np.cos(theta) - 1
 
-    def delta_c2(self, theta):
+    @staticmethod
+    def delta_c2(theta):
         return np.cos(theta)**2 - 1
 
-    def gamma(self, k):
-        return np.sqrt(self.m**2 + k**2)/self.m
+    def gamma(self, comp, k):
+        return np.sqrt(self.m[comp]**2 + k**2)/self.m[comp]
 
-    def v_operator(self, k, theta):
-        return self.v/self.gamma(k)
+    # volume operator
+    def v_operator(self, comp, k, theta):
+        return self.v[comp]/self.gamma(comp, k)
 
-    def s_operator(self, k, theta):
-        return self.s*(1./2./self.gamma(k)*(1. + self.delta_c1(theta)/2.) + self.a/2.*np.abs(np.sin(theta)))
+    # surface operator
+    def s_operator(self, comp, k, theta):
+        return self.s[comp]*(1./2./self.gamma(comp, k)*(1. + self.delta_c1(theta)/2.) + self.a/2.*np.abs(np.sin(theta)))
 
-    def c_operator(self, k, theta):
-        return self.c/2./self.gamma(k)*(1. + self.delta_c1(theta)/2. + self.delta_c2(theta)/6.)
+    # curvature operator
+    def c_operator(self, comp, k, theta):
+        return self.c[comp]/2./self.gamma(comp, k)*(1. + self.delta_c1(theta)/2. + self.delta_c2(theta)/6.)
 
-    def momentum_part(self, x, T):  # after change of variables k -> m tan( pi/4 (x + 1)) and integration limits (0, inf) -> (-1, 1)
-        k = self.m * np.tan(np.pi/4*(x+1))
-        jacobian = self.m * np.pi/4. * (1 + np.tan(np.pi/4.*(x+1.))**2.)
-        return 4*np.pi*self.g/(2*np.pi*hbar)**3 * k**2*np.exp(-np.sqrt(self.m**2+k**2)/T) * jacobian
+    # integration over momentum term in thermal density
+    def momentum_part(self, comp, x, T):  # after change of variables k -> m tan( pi/4 (x + 1)) and integration limits (0, inf) -> (-1, 1)
+        k = self.m[comp] * np.tan(np.pi/4*(x+1))
+        jacobian = self.m[comp] * np.pi/4. * (1 + np.tan(np.pi/4.*(x+1.))**2.)
+        return 4*np.pi*self.g[comp]/(2*np.pi*hbar)**3 * k**2*np.exp(-np.sqrt(self.m[comp]**2+k**2)/T) * jacobian
 
-    def pPart(self, T, mu, p, Sigma, K):
+    # one-component pressure (antibaryons included as separate component)
+    def pPart(self, comp, T, mu, p, Sigma, K):
 
-        def main_part(x, theta):
-            k = self.m * np.tan(np.pi/4*(x+1))
-            return T*np.exp(mu/T - self.v_operator(k, theta)*p/T - self.s_operator(k, theta)*Sigma/T - self.c_operator(k, theta)*K/T)
+        def main_part(comp, x, theta):
+            k = self.m[comp] * np.tan(np.pi/4*(x+1))
+            return 2.*T*np.cosh(mu/T)*np.exp(-self.v_operator(comp, k, theta)*p/T - self.s_operator(comp, k, theta)*Sigma/T - self.c_operator(comp, k, theta)*K/T)
 
-        def int_func(x): return integrate.quadrature(lambda theta: self.momentum_part(
-            x, T)*main_part(x, theta)*np.sin(theta), 0., np.pi/2.)[0]
+        def int_func(x):
+            return integrate.quadrature(lambda theta: self.momentum_part(comp, x, T)*main_part(comp, x, theta)*np.sin(theta), 0., np.pi/2.)[0]
         return integrate.quad(int_func, -1., 1.)[0]
 
-    def SigmaPart(self, T, mu, p, Sigma, K):
+    # one-component Induced Surface Tension (antibaryons included as separate component)
+    def SigmaPart(self, comp, T, mu, p, Sigma, K):
 
-        def main_part(x, theta):
-            k = self.m * np.tan(np.pi/4*(x+1))
-            return self.alpha_p*T*self.R * (self.b + (1-self.b)/self.gamma(k)) * np.exp(mu/T - self.v_operator(k, theta)*p/T - self.s_operator(k, theta)*self.alpha*Sigma/T - self.c_operator(k, theta)*K/T)
+        def main_part(comp, x, theta):
+            k = self.m[comp] * np.tan(np.pi/4*(x+1))
+            return 2.*self.alpha_p*T*self.R[comp] * (self.b + (1-self.b)/self.gamma(comp, k)) * np.cosh(mu/T) * np.exp(-self.v_operator(comp, k, theta)*p/T - self.s_operator(comp, k, theta)*self.alpha*Sigma/T - self.c_operator(comp, k, theta)*K/T)
 
-        def int_func(x): return integrate.quadrature(lambda theta: self.momentum_part(
-            x, T)*main_part(x, theta)*np.sin(theta), 0., np.pi/2.)[0]
+        def int_func(x):
+            return integrate.quadrature(lambda theta: self.momentum_part(comp, x, T)*main_part(comp, x, theta)*np.sin(theta), 0., np.pi/2.)[0]
+
         return integrate.quad(int_func, -1., 1.)[0]
 
-    def KPart(self, T, mu, p, Sigma, K):
+    # one-component Induced Curvature Tension (antibaryons included as separate component)
+    def KPart(self, comp, T, mu, p, Sigma, K):
 
-        def main_part(x, theta):
-            k = self.m * np.tan(np.pi/4*(x+1))
-            return self.beta_p*T*self.R**2 * (self.b + (1-self.b)/self.gamma(k)) * np.exp(mu/T - self.v_operator(k, theta)*p/T - self.s_operator(k, theta)*self.alpha*Sigma/T - self.c_operator(k, theta)*self.beta*K/T)
+        def main_part(comp, x, theta):
+            k = self.m[comp] * np.tan(np.pi/4*(x+1))
+            return 2.*self.beta_p*T*self.R[comp]**2 * (self.b + (1-self.b)/self.gamma(comp, k)) * np.cosh(mu/T) * np.exp(-self.v_operator(comp, k, theta)*p/T - self.s_operator(comp, k, theta)*self.alpha*Sigma/T - self.c_operator(comp, k, theta)*self.beta*K/T)
 
-        def int_func(x): return integrate.quadrature(lambda theta: self.momentum_part(
-            x, T)*main_part(x, theta)*np.sin(theta), 0., np.pi/2.)[0]
+        def int_func(x):
+            return integrate.quadrature(lambda theta: self.momentum_part(comp, x, T)*main_part(comp, x, theta)*np.sin(theta), 0., np.pi/2.)[0]
         return integrate.quad(int_func, -1., 1.)[0]
 
     def EoS(self, root, T, *mu):
         p, Sigma, K = root
-        return (p - self.pPart(T, *mu, p, Sigma, K), Sigma - self.SigmaPart(T, *mu, p, Sigma, K), K - self.KPart(T, *mu, p, Sigma, K))
+        return (
+            p - sum(self.pPart(comp, T, mu[comp], p, Sigma, K) for comp in range(self.num_of_components)),
+            Sigma - sum(self.SigmaPart(comp, T, mu[comp], p, Sigma, K) for comp in range(self.num_of_components)),
+            K - sum(self.KPart(comp, T, mu[comp], p, Sigma, K) for comp in range(self.num_of_components))
+        )
 
-    def root(self, T, *mu, p0=[1., 1., 1.]):
-        return fsolve(self.EoS, p0, args=(T, *mu))
+    def root(self, T, *mu, p0=[1., 1., 1.], **kwargs):
+        return fsolve(self.EoS, p0, args=(T, *mu), **kwargs)
 
     def v_eff_ratio(self, T, mu, p, Sigma, K):
 
