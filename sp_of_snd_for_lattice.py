925b97626075743b435191fe8f36b90aa9640fd7
Kyrill Bugaev <bugaev@users.fias.science>
2021-06-21 11:59:58 +0200

    rootdata only calc, submit script for diff masses

--- sp_of_snd_for_lattice.py
+++ sp_of_snd_for_lattice.py
@@ -8,13 +8,15 @@ import sys
 
 g_M = float(sys.argv[1])
 g_B = float(sys.argv[2])
-mu = float(sys.argv[3])
+m_M = float(sys.argv[3])
+m_B = float(sys.argv[4])
+mu = float(sys.argv[5])
 
-print('init for mu: {}\t g_M: {}\t g_B: {}'.format(mu, g_M, g_B))
+print('init for mu: {}\t g_M: {}\t g_B: {}\t m_M: {}\t m_B: {}'.format(mu, g_M, g_B, m_M, m_B))
 print('************************************************************')
 
 folder = 'cs_sq_fulldata'
-filepath = 'cs_sq_fulldata_mixture_g_M_{}_g_B_{}_mu_{}_.csv'.format(g_M, g_B, mu)
+filepath = 'rootdata_mixture_g_M_{}_g_B_{}_m_M_{}_m_B_{}_mu_{}_.csv'.format(g_M, g_B, m_M, m_B, mu)
 
 print('Saving to ', filepath)
 print('************************************************************')
@@ -22,7 +24,7 @@ print('************************************************************')
 # mixture of nucleons, pions, heavy mesons and heavy baryons
 eos = Relativistic_ISCT(
     components=4,
-    m=[940., 140., 1000., 1500.],
+    m=[940., 140., m_M, m_B],
     g=[4., 3., g_M, g_B],
     eos='ISCT',
     R=0.39,
@@ -33,9 +35,9 @@ Tdata = np.linspace(15., 3000., 100)
 pdata = []
 Sigmadata = []
 Kdata = []
-entropydata = []
-densitydata = []
-cs_sq_data = []
+# entropydata = []
+# densitydata = []
+# cs_sq_data = []
 
 mu = [mu, 0., 0., mu]
 
@@ -47,18 +49,18 @@ for T in Tdata:
     pdata.append(p)
     Sigmadata.append(Sigma)
     Kdata.append(K)
-    entropydata.append(eos.splined_entropy(T, *mu))
-    densitydata.append(eos.splined_density_baryon(T, *mu))
-    cs_sq_data.append(eos.splined_speed_of_s_sq(T, *mu))
+    # entropydata.append(eos.splined_entropy(T, *mu))
+    # densitydata.append(eos.splined_density_baryon(T, *mu))
+    # cs_sq_data.append(eos.splined_speed_of_s_sq(T, *mu))
 
     df = pd.DataFrame({
-        'T':Tdata,
+        'T':Tdata[:len(pdata)],
         'p':pdata,
         'Sigma':Sigmadata,
         'K':Kdata,
-        'entropy':entropydata,
-        'density_baryon':densitydata,
-        'sp_of_snd_sq':cs_sq_data
+        # 'entropy':entropydata,
+        # 'density_baryon':densitydata,
+        # 'sp_of_snd_sq':cs_sq_data
     })
 
     df.to_csv(os.path.join(folder, filepath), index=False)
